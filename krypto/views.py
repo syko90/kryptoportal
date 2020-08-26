e14f9dfc6770360e4006d5e9f9cf789878591e5d
Maciej Kurantowicz <kurantowicz.maciej@gmail.com>
2020-08-26 08:53:34 +0200

    Dodanie modułu requests, json i odpytanie API, wykorzystanie pętli FOR do pracy na danych z API

--- krypto/views.py
+++ krypto/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import requests
+import json
 
 def home(request):
-    return render(request, 'home.html', {}) 
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request, 'home.html', {'api': api}) 
