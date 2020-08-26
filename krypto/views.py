35788b9454d0f40e9b814cc5f34cdd9c17a529aa
Maciej Kurantowicz <kurantowicz.maciej@gmail.com>
2020-08-26 10:51:59 +0200

    Dodanie linków dla paska nawigacyjnego, opcja wyszukiwania

--- krypto/views.py
+++ krypto/views.py
@@ -13,3 +13,7 @@ def home(request):
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
     return render(request, 'home.html', {'api': api, 'price': price}) 
+
+
+def prices(request):
+    return render(request, 'prices.html', {})
