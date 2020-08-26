8e39c1f8ca5b687ad92c39641a4106fec94c1f15
Maciej Kurantowicz <kurantowicz.maciej@gmail.com>
2020-08-26 19:37:04 +0200

    Gotowy do Hereoku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kryptoportal.wsgi
--- kryptoportal/settings.py
+++ kryptoportal/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'kryptoportal.urls'
@@ -119,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFIlesStorage'
+
+django_heroku.settings(locals())
